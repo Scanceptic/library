d742da27081dd3dca1adae8d42d6b87baf22028f
Nathan <urwin.nathan@gmail.com>
2024-07-01 17:09:36 +0100

    add initial library display func

--- script.js
+++ script.js
@@ -9,9 +9,33 @@ function Book(author, title, pages, read) {
 
 function addBooktoLibrary(author, title, pages, read) {
     let newBook = new Book(author, title, pages, read);
-    return newBook;
+    myLibrary.push(newBook);
 }
 
 function bookDisplay(myLibrary) {
-    document.createElement()
+    const container = document.querySelector("card-container");
+    const div = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("span");
+    const bookPages = document.createElement("span");
+    const bookRead = document.createElement("span");
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+    div.style.backgroundColor = "gainsboro";
+    div.style.width = "200px";
+    div.style.height = "150px";
+    div.style.border = "1px solid black";
+    for (let i = 0; i < myLibrary.length; i++) {
+        bookTitle.textContent = myLibrary[i].title;
+        bookAuthor.textContent = myLibrary[i].author;
+        bookPages.textContent = myLibrary[i].pages;
+        bookRead.textContent = myLibrary[i].read;
+        div.appendChild(bookTitle);
+        div.appendChild(bookAuthor);
+        div.appendChild(bookPages);
+        div.appendChild(bookRead);
+        container.appendChild(div);
+    }
 }
